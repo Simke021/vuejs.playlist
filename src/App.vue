93d6ceb77f0215ad2993b946d0d83ccf475864e0
Aleksandar Ljubišić <simke20@yahoo.com>
2018-09-19 16:48:59 +0200

    Events ( child to parent )

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <app-header v-bind:title="title"></app-header>
+        <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
         <app-main v-bind:ninjas="ninjas"></app-main>
         <app-footer v-bind:title="title"></app-footer>
     </div>
@@ -29,6 +29,11 @@ export default {
       ],
       title: "Vue Ninjas"
     };
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle;
+    }
   }
 };
 </script>
